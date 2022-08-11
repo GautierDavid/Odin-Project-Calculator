fc2eabef0998a1f7dc3c8178ad8c4bbd45ab9e3e
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-11 16:55:01 +0200

    multiple operations in a row works correctly

--- script.js
+++ script.js
@@ -15,89 +15,84 @@ let operator2 = '';
 let num1 = '';
 let num2 = '';
 let result = '';
-let needToReset = false;
 
 
 
 function displayNumber(e) {
+    // reset at the start
     if(resultDisplay.innerHTML === '0') resultDisplay.innerHTML = '';
+    // reset si on a le premier operateur et que num2 est vide
+    else if (operator1 !== "" && num2 === "") resultDisplay.innerHTML = '';
 
-    if(operationPara.innerHTML !== "" && needToReset === true) {
-        resultDisplay.innerHTML = '';
-        needToReset = false;
-    }
-
+    // gère l'affichage
     resultDisplay.innerHTML += e.currentTarget.innerHTML;
+    // appel fonction setNumber pour attribuer les valeurs
+    setNumber(resultDisplay.innerHTML)
 }
 
-
-function reset() {
-    operator1 = '';
-    operator2 = '';
-    num1 = '';
-    num2 = '';
-    result = '';
-    needToReset = false;
-
-    operationPara.innerHTML = ''
-    resultDisplay.innerHTML = 0;
+function setNumber(num) {
+    // s'il n'y a pas de premier operator
+    if(operator1 === "") {
+        // num1 égal ce qui est affiché dans la div result
+        num1 = num;
+    } else {
+        // s'il y a un premier operator num2 égal ce qui est affiché dans la div result
+        num2 = num;
+    }
 }
 
 
 function displayOperator(e) {
     // in case the user click on = for the first operator
-    if(operator1 === "" && e.currentTarget.innerHTML === "=") return
+    if(num2 === "" && e.currentTarget.innerHTML === "=") return
+    // si clique sur operateur sans avoir rentré de chiffres
+    if(num1 === "") return
 
-    if(operator1 !== '') {
-        setOperator(e.currentTarget.innerHTML)
-    } else {
-        if(result !== '') {
-            num1 = result;
-            operator1 = e.currentTarget.innerHTML;
-            operationPara.innerHTML = `${num1} ${operator1}`
-            needToReset = true;
-        } else {
-            operator1 = e.currentTarget.innerHTML;
-            num1 = resultDisplay.innerHTML;
-            operationPara.innerHTML = `${num1} ${operator1}`
-            needToReset = true;
-        }
-    }    
+    // si num1 different vide et operator1 est vide OU num2 vide
+    if((operator1 === "" && num1 !== "") || num2 === "") {
+        // => operator1 égale bouton sur lequel on a cliqué
+        operator1 = e.currentTarget.innerHTML;
+        operationPara.innerHTML = `${num1} ${operator1}`;
+    // si num2 différent vide
+    } else if(num2 !== "") {
+        // => operator2 égale bouton sur lequel on a cliqué
+        operator2 = e.currentTarget.innerHTML;
+        // appel la fonction pour préparer le calcul
+        setOperator();
+    }
 }
 
 
-function setOperator(operator) {
-    num2 = resultDisplay.innerHTML;
-    operator2 = operator;
-    
+function setOperator() {
+    // si essaye de diviser par 0
     if(operator1 === '/' && (num1 === "0" || num2 === "0")) {
+        // message d'erreur
         alert('You can\'t divide by 0!');
-        reset();
+        return
+    } 
+
+    let result = operate(operator1, num1, num2);
+
+    // if result is a decimal, round to two decimals
+    if(result % 1 !== 0) result = result.toFixed(2);
+    console.log(operator2)
+    if(operator2 === '=') {
+        operationPara.innerHTML =  `${num1} ${operator1} ${num2} ${operator2}`
+        num1 = result;
     } else {
-        let result = operate(operator1, num1, num2);
-
-        // if result is a decimal, round to two decimals
-        if(result % 1 !== 0) result = result.toFixed(2);
-        
-        if(operator2 === '=') {
-            operationPara.innerHTML =  `${num1} ${operator1} ${num2} ${operator2}`
-        } else {
-            operator1 = operator2;
-            num1 = result;
-            operationPara.innerHTML = `${num1} ${operator1}`
-        }
-
-        
-        operator2 = '';
-        resultDisplay.innerHTML = result;
-
-        needToReset = true;
+        console.log("hi")
+        operator1 = operator2;
+        num1 = result;
+        operationPara.innerHTML = `${num1} ${operator1}`
     }
-    
+
+    num2 = '';
+    resultDisplay.innerHTML = num1;
 }
 
 
 function operate(operator, num1, num2) {
+    // translate string to number 
     num1 = Number(num1);
     num2 = Number(num2);
     switch(operator) {
@@ -111,6 +106,18 @@ function operate(operator, num1, num2) {
             return divide(num1, num2);
     }
 }
+
+
+function reset() {
+    operator1 = '';
+    operator2 = '';
+    num1 = '';
+    num2 = '';
+    operationPara.innerHTML = ''
+    resultDisplay.innerHTML = 0;
+}
+
+
 
 
 const add = function(a, b) {
